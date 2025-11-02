a84c49ba9fbe2072da241cd249312e8d1d21f176
Swapnil Nimbalkar <swapnilnimbalkarofficial@gmail.com>
2025-11-02 22:36:04 +0530

    Add files via upload

---
+++ Second.py
@@ -0,0 +1,19 @@
+#print("Hello Swapnil")
+
+# num=int(input("Enter a number:"))
+# if num<0:
+#     print("Negative")
+# if num>0:
+#     print("Positive")
+
+# if num==0:
+#     print("zero")
+
+
+name=input("enter your name:")
+password=input("enter your password:")
+
+if name=="swapnil" and password=="1212":
+    print("Welcome Swapnil")
+else:
+    print("Unsername or Password is incorrect")
---
+++ elif.py
@@ -0,0 +1,14 @@
+#elif project
+
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))      
+if age<18:
+    print("You are a minor")
+elif age==18:
+    print("You are just 18 years old")
+else:
+    print("You are an adult")
+
+
+
+    
